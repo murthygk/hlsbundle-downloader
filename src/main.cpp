aa6b90e517c84b085a702c2c9ef9125067de868f
KrishnaGudipati <>
2018-08-14 20:36:34 -0400

    Moved downloading into a function. Tested downloading stream file.

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,26 @@
 
 using namespace std;
 
-int main(void)
-{
+const string playlist = "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8";
+
+void downloadPlayListItem(const char* url, FILE* outputFile) {
     CURL *curl;
     CURLcode res;
-    string readBuffer;
-    FILE *outfile;
-    outfile = fopen("outfile", "w");
 
     curl = curl_easy_init();
     if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8");
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, outfile);
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, outputFile);
         res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
-
-        cout << readBuffer << endl;
     }
+}
+
+int main(void)
+{
+    FILE *outfile;
+    outfile = fopen("outfile-new", "w");
+    string url = string("https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/gear0/fileSequence176.aac");
+    downloadPlayListItem(url.c_str(), outfile);
     return 0;
 }
