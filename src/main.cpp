0a2cf4fe60551b4575e59c62628683795d65b2ae
KrishnaGudipati <>
2018-08-14 21:10:48 -0400

    Writing downloaded file using ofstream; Callback method added to supply to curl to send data to accumulate in the buffer. Finally from the buffer the data is appended to the output file using ofstream.

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,52 @@
 //  Created by Krishna Gudipati on 8/14/18.
-//  Copyright © 2018 The Weather Channel. All rights reserved.
 
 #include <iostream>
-#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
 #include <curl/curl.h>
+
 
 using namespace std;
 
-const string playlist = "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8";
+const string playlistUrl = "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8";
 
-void downloadPlayListItem(const char* url, FILE* outputFile) {
-    CURL *curl;
-    CURLcode res;
+void downloadPlayListItem(const char* url);
 
-    curl = curl_easy_init();
-    if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, outputFile);
-        res = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
+ofstream hlsstream;
+
+int main(void) {
+    hlsstream.open("fileSequence176.aac", ios::out | ios::app | ios::binary);
+    if (hlsstream.is_open()) {
+        string urlPath = string("https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/gear0/fileSequence176.aac");
+        downloadPlayListItem(urlPath.c_str());
     }
+
+    return 0;
 }
 
-int main(void)
+static size_t curlCallBack(void *curlData, size_t size, size_t receievedSize, void *writeToFileBuffer)
 {
-    FILE *outfile;
-    outfile = fopen("outfile-new", "w");
-    string url = string("https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/gear0/fileSequence176.aac");
-    downloadPlayListItem(url.c_str(), outfile);
-    return 0;
+    ((string*) writeToFileBuffer)->append((char *) curlData, size * receievedSize);
+    return size * receievedSize;
+}
+
+void downloadPlayListItem(const char* url) {
+
+    CURL *curl;
+    CURLcode res;
+    std::string readBuffer;
+
+    curl = curl_easy_init();
+    if (curl != NULL) {
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlCallBack);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+
+        hlsstream << readBuffer << endl;
+        std::cout << readBuffer << endl;
+    }
 }
